0ad672399f7b97b511ce2b1202b791e0343df21b
Eduardo dos Santos <perfilprofissional.Eduardo@gmail.com>
2026-04-19 01:40:44 -0300

    ATUALIZAÇÃO DO ARQUIVO V1
    Comecei a testar comandos da biblioteca.

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <gtk/gtk.h>
 
 int main(int argc, char *argv[]) {
+    //Variável auxiliar
     GtkWidget *window;
+    GtkWidget *logo_univasf;
 
-    // Inicializa o GTK
     gtk_init(&argc, &argv);
-
-    // Cria uma nova janela
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    
-    // Define o título e o tamanho inicial da janela
     gtk_window_set_title(GTK_WINDOW(window), "Sistema Hospitalar - Triagem");
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
-
-    // Conecta o botão "X" (fechar) para encerrar o programa corretamente
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    // Mostra a janela na tela
+    // 1. CRIA A LOGO
+    logo_univasf = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(logo_univasf), "<span font='40' weight='bold' height='20'>UNIVASF</span>");
+    gtk_widget_set_halign(logo_univasf, GTK_ALIGN_START);
+    
+    gtk_container_add(GTK_CONTAINER(window), logo_univasf);
+
+    // 3. MOSTRA A JANELA NA TELA (Sempre deve ser a última coisa antes do loop)
     gtk_widget_show_all(window);
 
     // Inicia o loop principal (fica esperando você clicar em algo)
