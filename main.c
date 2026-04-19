9fd4dd0a86d0d4c76ed640bac40b4ea27788b190
Eduardo dos Santos <perfilprofissional.Eduardo@gmail.com>
2026-04-19 12:24:11 -0300

    ADICIONAR IMAGEM LOGO UNIVASF V1.1
    Foi substituído a logo da UNIVASF em texto pela imagem da UNIVASF, onde teve sua largura e altura alterados e criado um sistema para monitoramento, caso a imagem seja deletada.

--- main.c
+++ main.c
@@ -3,20 +3,38 @@
 int main(int argc, char *argv[]) {
     //Variável auxiliar
     GtkWidget *window;
-    GtkWidget *logo_univasf;
+    GtkWidget *janela_logo; //Adicionando uma váriavel auxiliar para logo
+
+    //Variáveis para mexer na memória da imagem;
+    GdkPixbuf *imagem_original;
+    GdkPixbuf *imagem_redimensionada;
+    GError *erro = NULL;
 
     gtk_init(&argc, &argv);
+    
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Sistema Hospitalar - Triagem");
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    // 1. CRIA A LOGO
-    logo_univasf = gtk_label_new(NULL);
-    gtk_label_set_markup(GTK_LABEL(logo_univasf), "<span font='40' weight='bold' height='20'>UNIVASF</span>");
-    gtk_widget_set_halign(logo_univasf, GTK_ALIGN_START);
-    
-    gtk_container_add(GTK_CONTAINER(window), logo_univasf);
+    //criando minha imagem + identificação de erro
+    imagem_original = gdk_pixbuf_new_from_file("univasf_logo.png", &erro);
+
+    //Identificação de erro do carregamento da imagem + redimensionamento da imagem
+    if (erro != NULL){
+        g_print("Erro ao carregar a imagem");
+        g_error_free(erro);
+    }else{
+        imagem_redimensionada = gdk_pixbuf_scale_simple(imagem_original, 250, 120, GDK_INTERP_BILINEAR);
+        janela_logo = gtk_image_new_from_pixbuf(imagem_redimensionada);
+
+        //Limpar os pixbufs da memória, importante para não tracar o PC!
+        g_object_unref(imagem_original);
+        g_object_unref(imagem_redimensionada);
+
+        //Adicionar a imagem redimensionada na tela
+        gtk_container_add(GTK_CONTAINER(window), janela_logo);
+    }
 
     // 3. MOSTRA A JANELA NA TELA (Sempre deve ser a última coisa antes do loop)
     gtk_widget_show_all(window);
