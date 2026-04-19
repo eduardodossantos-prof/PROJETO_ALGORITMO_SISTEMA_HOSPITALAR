989da51267ed6c36e7d187e2c2ba01e57fe2418f
Eduardo dos Santos <perfilprofissional.Eduardo@gmail.com>
2026-04-19 16:08:59 -0300

    SHA V.2.0
    Na segunda versão da primeira tela foi montado a parte do quadrado, desenhando, pintando, e alterando o posicionamento do quadrado.

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
 #include <gtk/gtk.h>
 
+gboolean desenhar_quadrado(GtkWidget *widget, cairo_t *cr, gpointer data)
+{
+    cairo_set_source_rgb(cr, 0.85, 0.85, 0.85);
+    cairo_rectangle(cr, 100, 50, 300, 220);
+    cairo_fill(cr);
+    return FALSE;
+}
+
 int main(int argc, char *argv[]) {
     //Variável auxiliar
     GtkWidget *window;
     GtkWidget *janela_logo; //Adicionando uma váriavel auxiliar para logo
     GtkWidget *fixed; 
+    GtkWidget *area_desenho;
+    GtkWidget *area_desenho_2;
+    GtkWidget *area_desenho_3;
 
     //Variáveis para mexer na memória da imagem;
     GdkPixbuf *imagem_original;
@@ -36,10 +47,26 @@ int main(int argc, char *argv[]) {
 
         //Adicionar a imagem redimensionada na tela
         gtk_container_add(GTK_CONTAINER(window), fixed);
-        gtk_fixed_put(GTK_FIXED(fixed), janela_logo, 20, 530);
+        gtk_fixed_put(GTK_FIXED(fixed), janela_logo, 20, 670);
         gtk_container_add(GTK_CONTAINER(window), janela_logo);
     }
 
+    //Cria a área em branco para desenhar  
+    area_desenho = gtk_drawing_area_new();
+    area_desenho_2 = gtk_drawing_area_new();
+    area_desenho_3 = gtk_drawing_area_new();
+    //Define a largura e a altura máxima que a área vai ocupar
+    gtk_widget_set_size_request(area_desenho, 2000, 2000);
+    gtk_widget_set_size_request(area_desenho_2, 2000, 2000);
+    gtk_widget_set_size_request(area_desenho_3, 2000, 2000);
+    //Conectar o evento "draw" desenhar com a função "desenhar_quadrado" 
+    g_signal_connect(area_desenho, "draw", G_CALLBACK(desenhar_quadrado), NULL);    
+    g_signal_connect(area_desenho_2, "draw", G_CALLBACK(desenhar_quadrado), NULL);
+    g_signal_connect(area_desenho_3, "draw", G_CALLBACK(desenhar_quadrado), NULL);
+    //Coloca o quadrado na tela usando o GtkFixed
+    gtk_fixed_put(GTK_FIXED(fixed), area_desenho, 80, 180);
+    gtk_fixed_put(GTK_FIXED(fixed), area_desenho_2, 430, 180);
+    gtk_fixed_put(GTK_FIXED(fixed), area_desenho_3, 780, 180);
     // 3. MOSTRA A JANELA NA TELA (Sempre deve ser a última coisa antes do loop)
     gtk_widget_show_all(window);
 
